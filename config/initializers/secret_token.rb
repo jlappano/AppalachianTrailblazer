b35057854f6b7f9021b0333c623f4cdae8b24475
Julie lappano <julie.lappano@gmail.com>
2013-12-11 16:11:42 -0500

    app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrailBlazer::Application.config.secret_key_base = 'f4caf5eccd0eaa6621172e698fefa67364a4b4b8b9396433069ed68290c2074b3b5464063bc0704d255b08df342b0dde171befe9b720cce583da2121dc96a190'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrailBlazer::Application.config.session_store :cookie_store, key: '_TrailBlazer_session'
