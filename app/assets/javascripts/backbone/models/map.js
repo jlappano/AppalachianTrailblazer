7e608eca910220443fb0096ea89c9f37d85ca65d
Julie lappano <julie.lappano@gmail.com>
2014-01-08 15:56:41 -0500

    updated map with set fix

--- app/assets/javascripts/backbone/models/map.js
+++ app/assets/javascripts/backbone/models/map.js
@@ -7,10 +7,6 @@ var Map = Backbone.Model.extend({
   initialize: function(){
     console.log("map model loaded");
     this.set({center: new google.maps.LatLng(40.612732, -75.912438), mapTypeId: google.maps.MapTypeId.TERRAIN});
-    // this.center = new google.maps.LatLng(40.612732, -75.912438);
-    // this.mapTypeId = google.maps.MapTypeId.TERRAIN;
-
-    //center, mapTypeId
 //plots coordinates for Polyline
     this.shelterCoordinates = [
       new google.maps.LatLng(34.62673, -84.193656),
