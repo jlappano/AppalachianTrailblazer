95ac93fea8035a9cbcc7225d8652e05f5293009d
Julie lappano <julie.lappano@gmail.com>
2014-01-10 17:59:50 -0500

    morale bar work with occasional errors

--- app/assets/javascripts/backbone/views/game_view.js
+++ app/assets/javascripts/backbone/views/game_view.js
@@ -6,14 +6,43 @@ var GameView = Backbone.View.extend({
     this.template = _.template($("script.game").html());
     var shelter = this.model.get("shelter");
     shelter.getAdventurePrompt();
-    this.render();
+    // this.barRender();
+    this.render(); 
+    this.changeBar();
   },
+
+  // barRender: function() {
+
+  // },
+
   render: function() {
     this.$el.html(this.template());
+    // this.changeBar();
   },
 
   events: {
     "click button": "renderAnswer",
+  },
+
+  changeBar: function() {
+
+      var progress = this.$(".bar-percentage");
+      // console.log(progress.attr('data-percentage'));
+      var percentage = Math.ceil(progress.attr('data-percentage'));
+      console.log("percent");
+      console.log(percentage);
+      $({countNum: 0}).animate({countNum: percentage}, {
+        duration: 1000,
+        easing:'linear',
+        step: function() {
+        // What todo on every count
+        console.log(this.countNum);
+        var pct = Math.ceil(this.countNum) + '%';
+        console.log("pct");
+        console.log(pct);
+        progress.text(pct) && progress.siblings().children().css('width',pct);
+      }
+    });
   },
 
   renderAnswer: function(e) {
